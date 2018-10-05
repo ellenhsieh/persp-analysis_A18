77c70fc414e1659d1c335952f3c5dde2cc0891ef
ellenhsieh <43705293+ellenhsieh@users.noreply.github.com>
2018-10-05 17:29:17 -0500

    Update DescrStat.py

--- Assignments/A1/DescrStat.py
+++ Assignments/A1/DescrStat.py
@@ -14,8 +14,8 @@ def DescrStat(datafile):
     data = np.loadtxt(datafile)
     data_mean = data.mean()
     data_std = data.std()
-    data_var = data.variance()
-
-    print('Mean of the data is:', data_mean)
-    print('Standard deviation of the data is:', data_std)
-    print('Variance of the data is:', data_var)
+    data_var = data.var()
+    print(’Mean of the data is:’, data_mean)
+    print(’Standard deviation of the data is:’, data_std)
+    print(’Variance of the data is:’, data_var)
+    return data_mean, data_std, data_var
